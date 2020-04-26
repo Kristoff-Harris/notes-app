11881ae7d13ee5acb63a41e0fce7df83b0b98d59
Kristoff Harris <cjh6470@rit.edu>
2020-04-26 15:27:33 -0400

    Adding code to aquire title and body for notes  via command line args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,60 @@
 const chalk = require('chalk');
-const yargs = require('yargs')
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2]
+// customize Yargs version 
+yargs.version('1.1.0');
 
-console.log(yargs.argv)
 
-if (command === 'add'){
-    console.log('Adding note!');
-} else if (command === 'remove'){
-    console.log("deleting note!")
-} else {
-    console.log("not a valid choice")
-}
+// Create add command 
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'          // Demands that the title argument passed in is of type 'string'
+        },
+        body:{
+            describe: 'Contents of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body);
+    }
+
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    hanlder: function (){
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List out all notes',
+    handler: function(){
+        console.log('Listing out notes...')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'showing all of our notest',
+    handler: function(){
+        console.log('starting the read of all notes...')
+    }
+})
+
+
+yargs.parse();
 
 //console.log(myNote);
 
